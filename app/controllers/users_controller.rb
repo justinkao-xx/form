94c10b4a13e66e65c611f7d0e36d3ecb6ef1599d
Kaspar <veske@tlu.ee>
2013-11-18 23:54:34 +0200

    User is now created into the database

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,11 +5,11 @@ class UsersController < ApplicationController
   end
   
   def create
-    @user = User.new(params[user_params])
+    @user = User.new(user_params)
       if @user.save
         sign_in @user
         flash[:success]= "Registration success!"
-        redirect_to @user
+        redirect_to user_path(:user)
       else
         render 'new'
      end 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,14 @@
 class User < ActiveRecord::Base
   has_secure_password
   
-  before_save { self.email = email.downcase }
+  before_validation :downcase_email
   validates :name, presence: true, length: {maximum: 50}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false}
   validates :password, length: {minimum: 3}
+  
+private
+  def downcase_email
+    self.email = self.email.downcase if self.email.present?
+  end
 end
