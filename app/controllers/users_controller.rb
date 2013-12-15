9d2f924a859d33624e47f873ea439bcfcbffd3a5
Kaspar <veske@tlu.ee>
2013-12-15 17:32:19 +0200

    Admin users can now delete other users.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
-  before_action :signed_in_user, only: [:index,:edit,:update]
+  before_action :signed_in_user, only: [:index,:edit,:update, :destroy]
   before_action :correct_user, only: [:edit,:update]
+  before_action :admin_user,     only: :destroy
   
   def index
     @users = User.all
@@ -39,10 +40,19 @@ class UsersController < ApplicationController
       render 'edit'
     end
   end
+  
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User Deleted."
+    redirect_to users_url
+  end
   private
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
+   def admin_user
+      redirect_to(root_url) unless current_user.admin?
+    end
   # Redirecting not logged in user etc.
   def signed_in_user
     unless signed_in?
