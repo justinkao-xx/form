dcdd40c9318d12294b8794ebd9485a8ed8ad5279
David Southard <nacengineer@gmail.com>
2013-12-17 11:47:23 -0600

    fix errors

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,40 +1,42 @@
 class PostsController < ApplicationController
- 
+
   def index
     @post = current_user.posts.build
   end
-  
+
   def show
-    
+
   end
+
   def new
     @post = current_user.posts.build
   end
+
   def create
      @post = current_user.posts.build(post_params)
     if @post.save
       flash[:success] = "Post created!"
       redirect_to current_user
     else
-      render posts_new_path
+      render :new
     end
   end
-  
+
   def destroy
      @post = Post.find params[:id]
-     
+
      if @post.destroy
-      redirect_to posts_new_path, notice: "Post deleted."
+      redirect_to root_path, notice: "Post deleted."
      else
-      redirect_to posts_new_path, alert: "Couldn't delete Post: #{@post.errors.full_messages}"
+      redirect_to root_path, alert: "Couldn't delete Post: #{@post.errors.full_messages}"
   end
-    
+
   end
- 
+
   private
- 
+
   def post_params
     params.require(:post).permit(:content)
   end
-  
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,17 @@
 Harjutus::Application.routes.draw do
-  
-  resources :posts, only: [:new, :create, :destroy]
+
+  resources :posts,    only: [:index, :show, :new, :create, :destroy]
   resources :users
   resources :sessions, only: [:new, :create, :destroy]
-  
-  get "posts/new"
-  get "form/home"
+
+  get  "form/home"
   post "form/home"
-  get "form/converter"
+  get  "form/converter"
   post "form/converter"
-  get "form/about"
+  get  "form/about"
+
   root 'form#home'
-  #require 'net/http'
+
   match "/signout", to: 'sessions#destroy', via: :delete
-  
+
 end
