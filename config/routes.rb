146c022faf3f9fb8e2efe1799bcbb71afb7ee2da
Kaspar <veske@tlu.ee>
2014-01-10 17:33:41 +0200

    Made converter work

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Harjutus::Application.routes.draw do
   post "form/converter"
   get "form/about"
   root 'form#home'
-  #require 'net/http'
+  require 'net/http'
   match "/signout", to: 'sessions#destroy', via: :delete
   
 end
